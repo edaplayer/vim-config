f3be370a9195174854db9a3667179dcedc421e59
edaplayer <edaplayer@163.com>
2020-06-08 15:16:56 -0700

    Add .mybashrc

---
+++ init.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#########################################################################
+# File Name: install.sh
+# Author: Edward.Tang
+# mail:   @163.com
+# Created Time: Mon 08 Jun 2020 09:01:00 AM PDT
+#########################################################################
+if grep -q -e mybashrc .bashrc ;then
+	echo Nothing to do.
+else
+	echo "Add mybashrc."
+	echo >> .bashrc
+	echo "source .mybashrc" >> .bashrc
+fi
