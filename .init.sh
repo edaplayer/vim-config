4b6de94de8305b2ff3b4194f11073efbc6819077
tangfuyuan <tangfuyuan@justtide.com>
2020-09-22 16:34:11 +0800

    rename init.sh to .init.sh, copy vim file

--- .init.sh
+++ .init.sh
@@ -5,10 +5,12 @@
 # mail:   @163.com
 # Created Time: Mon 08 Jun 2020 09:01:00 AM PDT
 #########################################################################
-if grep -q -e mybashrc .bashrc ;then
+if grep -q -e mybashrc ~/.bashrc ;then
 	echo Nothing to do.
 else
 	echo "Add mybashrc."
 	echo >> .bashrc
-	echo "source ~/.mybashrc" >> .bashrc
+	echo "source ~/.mybashrc" >> ~.bashrc
 fi
+
+cp *.vimr* ~/
